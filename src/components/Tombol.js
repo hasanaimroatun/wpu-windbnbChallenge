ce8060141ad8634f2dec6b99dce821368725c75b
hasanaimroatun <imroatunhasana@gmail.com>
2022-07-25 10:50:05 +0700

    menambahkan toogle dan mengimport pencarian.js

--- src/components/Tombol.js
+++ src/components/Tombol.js
@@ -1,17 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {MdSearch} from 'react-icons/md'
 import imgBnb from './icon/logo.png'
 import './myStyle.css'
+import Pencarian from './Pencarian'
 
-function Tombol() {
+function Tombol({tambahLokasi, tambahTamu}) {
+  const [isOpen, setIsOpen] = useState(false)
+  const [pembungkusLBox, setPembungkusLBox] = useState('none')
+
+  const toggleIsOpen = () => {
+      setIsOpen(!isOpen)
+      setPembungkusLBox('lbox')
+  }
+
   return (
     <div>
+        <Pencarian isOpen={isOpen} toggleIsOpen={toggleIsOpen} pembungkusLBox={pembungkusLBox}/>
         <span>
             <img src={imgBnb} alt="logo" className='bnb'/>
         </span>
         <span className='containerTombol'>
-          <button className='kotak1'>Add location</button>
-          <button className='kotak2'>Add guests</button>
+          <button onClick={toggleIsOpen} className='kotak1'>{tambahLokasi}</button>
+          <button className='kotak2'>{tambahTamu}</button>
           <button  type='submit' className='kotak3'>
               <MdSearch size='1.5em' color='#eb5757'/>
           </button>
