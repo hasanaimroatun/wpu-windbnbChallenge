e8ff7d445335363de9d1bf22160508bc2135a5c5
hasanaimroatun <imroatunhasana@gmail.com>
2022-07-25 19:10:15 +0700

    mengimport lokasi.js

--- src/components/Pencarian.js
+++ src/components/Pencarian.js
@@ -1,30 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './myStyle.css'
 import {MdSearch} from 'react-icons/md'
+import Lokasi from './Lokasi'
 
 const Pencarian = ({isOpen, toggleIsOpen, pembungkusLBox}) => {
- 
+    const [pilihLokasi, setPilihLokasi] = useState('-Pilih lokasi-')
+
   return (
     <div>
       {isOpen?
         <div className={pembungkusLBox}>
           <div className='containerTombolLBox'>
             <span>
-              <select name="lokasi" id="lokasi">
-                <option value="lokasi1">coba</option>
-                <option value="lokasi2">coba2</option>
-              </select>
+              <button>
+                <div>LOCATION</div>
+                <div>{pilihLokasi}</div>
+              </button>
+            </span>
+            <span>
+              <button>
+                
+              </button>
             </span>
             <span>
               <button  onClick={toggleIsOpen} type='submit' className='kotak3'>
                 <MdSearch size='1.5em' color='#eb5757'/>
               </button>
             </span>
+
+            <Lokasi setPilihLokasi={setPilihLokasi}/>
+
           </div>
         </div>
         : null
       }
-        
     </div>
   )
 }
