ec3cc5fba65a5aa6737a25845977b0bc59e9ee3a
hasanaimroatun <imroatunhasana@gmail.com>
2022-07-26 15:17:34 +0700

    menambahkan style

--- src/components/Pencarian.js
+++ src/components/Pencarian.js
@@ -1,34 +1,45 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import './myStyle.css'
 import {MdSearch} from 'react-icons/md'
 import Lokasi from './Lokasi'
+import Guests from './Guests'
 
 const Pencarian = ({isOpen, toggleIsOpen, pembungkusLBox}) => {
-    const [pilihLokasi, setPilihLokasi] = useState('-Pilih lokasi-')
+    const [pilihLokasi, setPilihLokasi] = useState(<div className='tambahLokasiDanTamu'>Add location</div>)
+    const [tambahkanTamu, setTambahkanTamu] = useState()
 
+    useEffect(() => {
+      setTambahkanTamu(<div className='tambahLokasiDanTamu'>Add guests</div>)
+    }, [])
+    
   return (
     <div>
       {isOpen?
         <div className={pembungkusLBox}>
           <div className='containerTombolLBox'>
             <span>
-              <button>
-                <div>LOCATION</div>
+              <button className='tombolLokasi'>
+                <div className='lokasiDanTamu'>LOCATION</div>
                 <div>{pilihLokasi}</div>
               </button>
             </span>
             <span>
-              <button>
-                
+              <button className='tombolTamu'>
+                <div className='lokasiDanTamu'>GUESTS</div>
+                <div>{tambahkanTamu}</div>
               </button>
             </span>
             <span>
-              <button  onClick={toggleIsOpen} type='submit' className='kotak3'>
-                <MdSearch size='1.5em' color='#eb5757'/>
+              <button  onClick={toggleIsOpen} type='submit' className='tombolPencarian'>
+                <MdSearch size='1.5em' color='#f2f2f2' className='gbrPencarian'/>
+                <span className='pencarian'>Search</span>
               </button>
             </span>
 
-            <Lokasi setPilihLokasi={setPilihLokasi}/>
+            <div>
+              <Lokasi setPilihLokasi={setPilihLokasi} />
+              <Guests setTambahkanTamu={setTambahkanTamu} />
+            </div>
 
           </div>
         </div>
