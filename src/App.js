470eb109248971c8b74fd15c402b010a07e235f8
hasanaimroatun <imroatunhasana@gmail.com>
2022-07-27 13:23:50 +0700

    mengimport file filter.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Card from './components/Card'
 import CreatedBy from './components/CreatedBy'
+import Filter from './components/Filter'
 
 function App() {
   return (
     <div>
       <Card />
       <CreatedBy />
+      <Filter />
     </div>
   )
 }
