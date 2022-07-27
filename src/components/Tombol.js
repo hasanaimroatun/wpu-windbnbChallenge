af43b94533081fedb80735b35b987b3b80268006
hasanaimroatun <imroatunhasana@gmail.com>
2022-07-27 13:23:18 +0700

    menambahkan fungsi handleTampilkanLokasi dan handleTampilkanTamu

--- src/components/Tombol.js
+++ src/components/Tombol.js
@@ -7,21 +7,67 @@ import Pencarian from './Pencarian'
 function Tombol({tambahLokasi, tambahTamu}) {
   const [isOpen, setIsOpen] = useState(false)
   const [pembungkusLBox, setPembungkusLBox] = useState('none')
+  const [tampilkanLokasi, setTampilkanLokasi] = useState('none')
+  const [tampilkanKategoriTamu, setTampilkanKategoriTamu] = useState('none')
+  const [tKategoriUsiaTamu, setTKategoriUsiaTamu] = useState('none')
+  const [tTombolKurang, setTTombolKurang] = useState('none')
+  const [tHasilTambahKurang, setTHasilTambahKurang] = useState('none')
+  const [tTombolTambah, setTTombolTambah] = useState('none')
 
   const toggleIsOpen = () => {
       setIsOpen(!isOpen)
       setPembungkusLBox('lbox')
   }
 
+  const handleTampilkanLokasi = () => {
+    setTampilkanLokasi('pilihanLokasi')
+    setTampilkanKategoriTamu('none')
+    setTKategoriUsiaTamu('none')
+    setTTombolKurang('none')
+    setTHasilTambahKurang('none')
+    setTTombolTambah('none')
+  }
+
+  const handleTampilkanTamu = () => {
+    setTampilkanKategoriTamu('kategoriTamu')
+    setTKategoriUsiaTamu('kategoriUsiaTamu')
+    setTTombolKurang('tombolKurang')
+    setTHasilTambahKurang('hasilTambahKurang')
+    setTTombolTambah('tombolTambah')
+    setTampilkanLokasi('none')
+  }
+  
   return (
     <div>
-        <Pencarian isOpen={isOpen} toggleIsOpen={toggleIsOpen} pembungkusLBox={pembungkusLBox}/>
+        <Pencarian 
+          isOpen={isOpen} 
+          toggleIsOpen={toggleIsOpen} 
+          pembungkusLBox={pembungkusLBox} 
+          tampilkanLokasi={tampilkanLokasi}
+          tampilkanKategoriTamu={tampilkanKategoriTamu}
+          tKategoriUsiaTamu={tKategoriUsiaTamu}
+          tTombolKurang={tTombolKurang}
+          tHasilTambahKurang={tHasilTambahKurang}
+          tTombolTambah={tTombolTambah}
+          handleTampilkanLokasi={handleTampilkanLokasi}
+          handleTampilkanTamu={handleTampilkanTamu}
+        />
         <span>
             <img src={imgBnb} alt="logo" className='bnb'/>
         </span>
         <span className='containerTombol'>
-          <button onClick={toggleIsOpen} className='kotak1'>{tambahLokasi}</button>
-          <button onClick={toggleIsOpen} className='kotak2'>{tambahTamu}</button>
+          <button 
+            onClick={() => {toggleIsOpen(); handleTampilkanLokasi()}} 
+            className='kotak1'
+          >
+              {tambahLokasi}
+          </button>
+          <button 
+            onClick={() => {toggleIsOpen(); handleTampilkanTamu()}} 
+            className='kotak2'
+          >
+            {tambahTamu}
+          </button>
           <button  type='submit' className='kotak3'>
               <MdSearch size='1.5em' color='#eb5757'/>
           </button>
