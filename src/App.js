9dee710c106676428bcc1ebecb41a4ea2fdaa5dc
hasanaimroatun <imroatunhasana@gmail.com>
2022-07-27 14:24:05 +0700

    menghapus import filter.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import Card from './components/Card'
 import CreatedBy from './components/CreatedBy'
-import Filter from './components/Filter'
 
 function App() {
   return (
     <div>
       <Card />
       <CreatedBy />
-      <Filter />
     </div>
   )
 }
