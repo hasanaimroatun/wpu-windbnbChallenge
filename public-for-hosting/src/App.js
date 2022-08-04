8b54cd2d7b9e29a9fe02200a0665d07c66f72028
hasanaimroatun <imroatunhasana@gmail.com>
2022-08-04 19:48:05 +0700

    menginstal firebase

---
+++ public-for-hosting/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Card from './components/Card'
+import CreatedBy from './components/CreatedBy'
+
+function App() {
+  return (
+    <div>
+      <Card />
+      <CreatedBy />
+    </div>
+  )
+}
+
+export default App
